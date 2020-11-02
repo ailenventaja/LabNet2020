14827c4e33d5a3e3598bb12c6cf32e1358651590
ailenventaja <63921625+ailenventaja@users.noreply.github.com>
2020-11-02 16:17:06 -0300

    Add files via upload

--- ejercicio3/WebApplicationej3/App_Start/BundleConfig.cs
+++ ejercicio3/WebApplicationej3/App_Start/BundleConfig.cs
@@ -8,20 +8,8 @@ namespace WebApplicationej3
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                   "~/Content/assets/mail/jqBootstrapValidation.js",
-                   "~/Content/assets/mail/products_me.js",
                    "~/Content/js/scripts.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
