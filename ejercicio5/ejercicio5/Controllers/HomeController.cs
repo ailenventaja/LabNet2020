60b27781ed97a064b89d7eacd4b955995e4ab74e
ailenventaja <ailuuvee@gmail.com>
2020-11-16 15:33:12 -0300

    Update HomeController.cs

--- ejercicio5/ejercicio5/Controllers/HomeController.cs
+++ ejercicio5/ejercicio5/Controllers/HomeController.cs
@@ -21,18 +21,5 @@ namespace ejercicio5.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
